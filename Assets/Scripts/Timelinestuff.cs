865405182779cc4e26d9434caee67f5c80b51535
chlovers <chloie_day@hotmail.com>
2024-11-19 22:27:51 -0400

    fixed the animation so it doesnt play right off the bat

--- Assets/Scripts/Timelinestuff.cs
+++ Assets/Scripts/Timelinestuff.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.Playables;
 
 public class Timelinestuff : MonoBehaviour
 {
     public PlayableDirector timeline;
+    
+
 
     private void OnTriggerEnter(Collider other)
     {
         timeline.Play();
+       
     }
 
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
